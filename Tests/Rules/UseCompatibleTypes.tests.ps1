af9ec6c8df5409fc914b46bb49c5a5091d609ac5
Kapil Borle <kborle@microsoft.com>
2017-06-05 13:13:35 -0700

    Add rule skeleton

---
+++ Tests/Rules/UseCompatibleTypes.tests.ps1
@@ -0,0 +1,17 @@
+﻿$directory = Split-Path -Parent $MyInvocation.MyCommand.Path
+$testRootDirectory = Split-Path -Parent $directory
+
+Import-Module PSScriptAnalyzer
+Import-Module (Join-Path $testRootDirectory "PSScriptAnalyzerTestHelper.psm1")
+
+Describe "UseCompatibleTypes" {
+    Context "When an incompatible type is given" {
+        It "Should detect the incompatible type" {
+            $def = @'
+[string]$x = "hello"
+'@
+            $violations = Invoke-ScriptAnalyzer -ScriptDefinition $def -IncludeRule "PSUseCompatibleTypes"
+            $violations.Count | Should Be 1
+        }
+    }
+}
